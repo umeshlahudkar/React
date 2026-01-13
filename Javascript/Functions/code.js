ce875c078219dff5ecedb234222ad4ea77c53eed
Umesh Lahudkar <99596032+umeshlahudkar@users.noreply.github.com>
2026-01-13 07:59:31 +0530

    basics of functions

--- Javascript/Functions/code.js
+++ Javascript/Functions/code.js
@@ -1,1 +1,23 @@
-console.log("555555");
+
+
+function PrintNumber(number) {
+
+  if(number < 0) {
+    console.log("number is less than zero");
+    return;
+  }
+
+  for(let i = 0; i <= number; i++) {
+    console.log(i);
+  }
+}
+
+//PrintNumber(15);
+
+function GetAverage(a, b, c) {
+  let avg = (a+b+c)/3;
+  return avg;
+}
+
+let avg = GetAverage(15, 15, 100);
+console.log(`Average is : ${avg}`);
